5e7c19657692b42671c860fa7ba92f97a1d83506
MrProLopstar <gosyar@gmail.com>
2024-03-27 23:53:26 +0300

    add lab4

--- src/panels/Lab4.jsx
+++ src/panels/Lab4.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Panel, PanelHeader, FormItem, ChipsInput, FormLayoutGroup, Input, Select, Button, Separator, Slider } from '@vkontakte/vkui';
+import { Panel, PanelHeader, FormItem, ChipsInput, FormLayoutGroup, Input, Select, Button, Separator, Slider, Textarea } from '@vkontakte/vkui';
 import { Icon24Back } from '@vkontakte/icons';
 import { getState, dispatch } from '../main.jsx';
 import { goBack } from '../store/router';
@@ -37,9 +37,7 @@ class Lab2 extends Component {
 		super(props);
 		this.state = {
 			select: 0,
-			M: 4,
-			N: 10,
-			n: 5,
+			lambda: 10,
 			//
 			alpha: 10,
 			beta: 0.6,
@@ -47,9 +45,6 @@ class Lab2 extends Component {
 			percent: 0.9,
 			sampleSize: 20,
 			//
-			lambdaMin: 0,
-			lambda: 0,
-			lambdaMax: 0,
 			arr: null
 		};
 	}
@@ -63,21 +58,40 @@ class Lab2 extends Component {
 		return Math.random()<p;
 	}
 
-	generateHyperGeometricSample = (N, n, K, sampleSize) => {
-        let sample = [];
-		for(let i=0; i<sampleSize; i++){
-			let sum = 0, p = K/N;
-			for(let j=1; j<=n; j++){
-				if(this.Bernoulli(p)){
-					sum++;
-					if(sum===K) break;
-				}
-				p = (K-sum)/(N-j);
-			}
-			sample.push(sum);
-		}
+	PoissonKnuth = (lambda) => {
+		let L = Math.exp(-lambda);
+		let k = 0;
+		let p = 1;
+		do {
+			k = k + 1;
+			let u = Math.random();
+			p *= u;
+		} while(p>L);
+		return k - 1;
+	};
+	generatePoissonSample = (lambda, sampleSize) => {
+		let sample = [];
+		for (let i = 0; i < sampleSize; i++) sample.push(this.PoissonKnuth(lambda));
 		return sample;
-    };
+	};
+	  
+	calculatePoissonEstimates = (sample,percent) => {
+		const mean = sample.reduce((acc, val) => acc + val, 0) / sample.length;
+		const variance = sample.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0) / (sample.length - 1);
+		const standardError = Math.sqrt(variance) / Math.sqrt(sample.length);
+		
+		const z = {
+			'0.9': 1.645,
+			'0.95': 1.96,
+			'0.99': 2.576
+		}[percent.toString()];
+		const marginOfError = z * standardError;
+		
+		return {
+			lambd: mean,
+			lambdaConfidenceInterval: [mean - marginOfError, mean + marginOfError]
+		};
+	};
 
     generateBetaSample = (alpha, beta, sampleSize) => {
 		let sample = [];
@@ -94,48 +108,51 @@ class Lab2 extends Component {
 	}
 
     calculation = () => {
-		const { select, M, N, n, alpha, beta, sampleSize, percent } = this.state;
-		let sample = [], estimates = {};
+		const { select, lambda, alpha, beta, sampleSize, percent } = this.state;
+		let sample = [], estimates = {}, answer = '';
 		
-		if (select === 0) {
-			if (n > N || M > N || M > n) return alert('Параметры n и M должны быть меньше или равны N, и M не должно превышать n.');
-			sample = this.generateHyperGeometricSample(N, n, M, sampleSize);
-			estimates = this.calculateEstimates(sample,(percent*100).toFixed(0));
+		if(select===0){
+			if(lambda<=0) return alert('Параметр λ должен быть больше нуля.');
+			sample = this.generatePoissonSample(lambda, sampleSize);
+			estimates = this.calculatePoissonEstimates(sample,percent);
+			answer = `Точечное значение c вероятность ${(percent*100).toFixed(0)}% λ*=${estimates.lambd}\n${estimates.lambdaConfidenceInterval[0].toFixed(4)} ≤ λ* ≤ ${estimates.lambdaConfidenceInterval[1].toFixed(4)}`;
 		} else if (select === 1) {
 			if (alpha <= 0 || beta <= 0) return alert('Параметры α и β должны быть больше нуля.');
 			sample = this.generateBetaSample(alpha, beta, sampleSize);
-			console.log(this.calculateBetaEstimates(alpha, beta, percent));
+			estimates = this.calculateBetaEstimates(sample, percent);
+			answer = `Точечная оценка параметра α: ${estimates.alpha.toFixed(4)}, ` +
+					 `Точечная оценка параметра β: ${estimates.beta.toFixed(4)}\n` +
+					 `Доверительный интервал для α: [${estimates.alphaConfidenceInterval[0].toFixed(4)}, ${estimates.alphaConfidenceInterval[1].toFixed(4)}], ` +
+					 `Доверительный интервал для β: [${estimates.betaConfidenceInterval[0].toFixed(4)}, ${estimates.betaConfidenceInterval[1].toFixed(4)}]`;
 		}
-	
-		let formattedSample = sample.map(value => ({ value: uuidv4(), label: this.resizeLength(value, 4) }));
 		this.setState({
-			arr: formattedSample,
+			arr: sample.map(value => ({ value: uuidv4(), label: this.resizeLength(value, 4) })),
 			renderKey: Math.random(),
+			answer,
 			...estimates
 		});
 	};
 
-	calculateBetaEstimates = (sample, alpha, beta, percent) => {
+	calculateBetaEstimates = (sample, percent) => {
 		const mean = jStat.mean(sample);
 		const variance = jStat.variance(sample);
 		
-		const alphaHat = mean * ((mean * (1 - mean)) / variance - 1);
-		const betaHat = (1 - mean) * ((mean * (1 - mean)) / variance - 1);
-
-		const standardErrorAlpha = /* расчет стандартной ошибки для альфа */;
-		const standardErrorBeta = /* расчет стандартной ошибки для бета */;
+		const alphaHat = mean*((mean*(1-mean))/variance-1);
+		const betaHat = (1-mean)*((mean*(1-mean))/variance-1);
 	  
-		const z = jStat.normal.inv(1 - percent / 2, 0, 1);
+		const sampleSize = sample.length;
+		const standardError = Math.sqrt(alphaHat*betaHat/((alphaHat+betaHat)**2*(alphaHat+betaHat+1)/sampleSize));
 	  
+		const z = jStat.normal.inv(1-percent/2, 0, 1);
 		const alphaInterval = [
-		  alphaHat - z * standardErrorAlpha,
-		  alphaHat + z * standardErrorAlpha
+		  alphaHat - z * standardError,
+		  alphaHat + z * standardError
 		];
 		const betaInterval = [
-		  betaHat - z * standardErrorBeta,
-		  betaHat + z * standardErrorBeta
+		  betaHat - z * standardError,
+		  betaHat + z * standardError
 		];
-	  
+		
 		return {
 		  alpha: alphaHat,
 		  beta: betaHat,
@@ -144,56 +161,40 @@ class Lab2 extends Component {
 		};
 	  };
 
-	calculateNormalEstimates = () => {
-		const { sampleSize } = this.state;
-		const sample = norm.slice(0, sampleSize);
+	  calculateNormalParameters = (sample) => {
+		const mean = jStat.mean(sample);
+		const variance = jStat.variance(sample);
+		const stdDeviation = Math.sqrt(variance);
+		return { mean, variance, stdDeviation };
+	  };
 
-		const mean = sample.reduce((acc, val) => acc + val, 0) / sample.length;
-		const variance = sample.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0) / (sample.length - 1);
-		
-		const zValues = {
-			'0.9': 1.645,
-			'0.95': 1.96,
-			'0.99': 2.576
-		};
-	
-		const intervalEstimates = this.calculateEstimates(sample, this.state.percent * 100);
-		
+	  analyzeNormalDistribution = () => {
+		const { percent } = this.state;
+		const sample = norm;
+		const { mean, variance, stdDeviation } = this.calculateNormalParameters(sample);
+		const confidenceLevel = percent;
+		const zValue = jStat.normal.inv(1-(1-confidenceLevel)/2, 0, 1);
+		const marginOfError = zValue*(stdDeviation/Math.sqrt(sample.length));
+		const confidenceInterval = [mean - marginOfError, mean + marginOfError];
+	  
+		const answer = `Среднее значение показателя динамометрии кисти: ${mean.toFixed(3)} кг/масса тела.\n` +
+					   `С вероятностью alpha = ${(confidenceLevel * 100).toFixed(0)}% данный показатель находится в пределах ${confidenceInterval[0].toFixed(3)} < показатель < ${confidenceInterval[1].toFixed(3)}.\n` +
+					   `Среднее квадратическое отклонение составляет ${stdDeviation.toFixed(3)}.\n` +
+					   `С вероятностью alpha = ${(confidenceLevel * 100).toFixed(0)}% среднее квадратическое отклонение находится в пределах ${confidenceInterval[0].toFixed(3)} < σ < ${confidenceInterval[1].toFixed(3)}.\n` +
+					   `Дисперсия показателя составляет D=${variance.toFixed(3)}.`;
+	  
 		this.setState({
-			lambdaMin: intervalEstimates.min,
-			lambda: intervalEstimates.lambda,
-			lambdaMax: intervalEstimates.max
+		  arr: sample.map(value => ({ value: uuidv4(), label: value.toFixed(3) })),
+		  renderKey: Math.random(),
+		  answer
 		});
-	};
-	
-	calculateEstimates = (sample, percent) => {
-		const mean = sample.reduce((acc, val) => acc + val, 0) / sample.length;
-		const standardError = Math.sqrt(sample.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0) / (sample.length - 1)) / Math.sqrt(sample.length);
-		const z = {
-			'90': 1.645,
-			'95': 1.96,
-			'99': 2.576
-		}[percent];
-	
-		const marginOfError = z * standardError;
-	
-		return {
-			min: mean - marginOfError,
-			max: mean + marginOfError,
-			lambda: mean
-		};
-	};
-	
-	normal = () => {
-		this.calculateNormalEstimates();
-	};
+	  };
 
 	render(){
 		const {title} = getState().app;
-		const {select,M,N,n,alpha,beta,arr,renderKey,percent,sampleSize,min,max,lambda} = this.state;
-		console.log(min,max,lambda)
+		const {select,lambda,alpha,beta,arr,renderKey,percent,sampleSize,answer} = this.state;
 		const sortedArr = arr?.slice().sort((a, b) => parseFloat(a.label) - parseFloat(b.label));
-		const titles = ['Гипергеометрический закон','Бета закон'];
+		const titles = ['Распределение Пуассона','Бета закон'];
 		return (
 			<Panel>
 				<PanelHeader before={<Icon24Back onClick={() => dispatch(goBack())}/>}>{title}</PanelHeader>
@@ -235,24 +236,10 @@ class Lab2 extends Component {
 									onChange={({target}) => this.setState({select: target.value==='' ? 0 : parseInt(target.value, 10)})}
 								/>
 							</FormItem>
-							<FormItem top='Размер совокупности N:'>
+							<FormItem top='λ:'>
 								<Input
-									value={N}
-									onChange={({ target }) => this.setState({N: target.value==='' ? 0 : parseInt(target.value, 10)})}
-								/>
-							</FormItem>
-						</FormLayoutGroup>
-						<FormLayoutGroup mode="horizontal" segmented>
-							<FormItem top='Количество "успехов" в совокупности M:'>
-								<Input
-									value={M}
-									onChange={({ target }) => this.setState({M: target.value==='' ? 0 : parseInt(target.value, 10)})}
-								/>
-							</FormItem>
-							<FormItem top='Размер выборки n:'>
-								<Input
-									value={n}
-									onChange={({ target }) => this.setState({n: target.value==='' ? 0 : parseInt(target.value, 10)})}
+									value={lambda}
+									onChange={({ target }) => this.setState({lambda: target.value==='' ? 0 : parseInt(target.value, 10)})}
 								/>
 							</FormItem>
 						</FormLayoutGroup>
@@ -274,7 +261,7 @@ class Lab2 extends Component {
 				/></FormItem>
 				<FormItem><Button rounded stretched onClick={this.calculation}>Рассчёт</Button></FormItem>
 				<Separator/>
-				<FormItem><Button rounded stretched onClick={this.normal}>Рассчёт Нормального распределения</Button></FormItem>
+				<FormItem><Button rounded stretched onClick={this.analyzeNormalDistribution}>Рассчёт Нормального распределения</Button></FormItem>
 				{arr && (
 					<div>
 						<FormItem top='Изначальный массив'>
@@ -289,6 +276,13 @@ class Lab2 extends Component {
 							<ChipsInput
 								disabled
 								value={sortedArr}
+								key={renderKey}
+							/>
+				  		</FormItem>
+				  		<FormItem top='Результат'>
+							<Textarea
+								disabled
+								value={answer}
 								key={renderKey}
 							/>
 				  		</FormItem>
